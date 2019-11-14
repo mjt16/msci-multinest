c17d363fc0d4c1dfefbbb2f021b1779f27938736
mjt16 <56415375+mjt16@users.noreply.github.com>
2019-11-14 14:56:40 +0000

    added multinest prelim

---
+++ multinest_ours_prelim.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 14:34:02 2019
+
+@author: mjt16
+"""
+
+# nested sampling script
+
+#importing modules
+from math import pi
+import numpy as np
+import matplotlib.pyplot as plt
+from __future__ import absolute_import, unicode_literals, print_function
+from pymultinest.solve import solve
+import os
+try: os.mkdir('chains')
+except OSError: pass
+
+# define frequency array
+freq = np.linspace(51.965332, 97.668457, 50)
+
+# functions
+def absorption(amp, x0, width): # signal 21cm absorption dip, defined as a negative gaussian
+    return -amp*np.exp((-(freq-x0)**2)/(2*width**2))
+
+def foreground(coeffs): # signal foreground
+    l = len(coeffs)
+    p = np.arange(0,l,1)
+    freq_arr = np.transpose(np.multiply.outer(np.full(l,1), freq))
+    pwrs = np.power(freq_arr, p)
+    ctp = coeffs*pwrs
+    return np.sum(ctp,(1)) 
+
+def addnoise(data, int_time): # add noise to signal
+    return data + data/(np.sqrt((freq[1]-freq[0])*(int_time)))
+
+# creating simulated foreground data using EDGES foreground; see edgesfit.py
+a0 = 47052.32658584365
+a1 = -1844.967333964787
+a2 = 29.265792826929875
+a3 = -0.21536464292060956
+a4 = 0.0006102144740830822
+sim_coeffs = np.array([a0,a1,a2,a3,a4]) # simulated foreground coeffs
+
+# creating simulated 21cm data
+sim_amp = 0.5 # amplitude
+sim_x0 = 78 # centre i.e. peak frequency
+sim_width = 8.1 # width
+
+int_time = 1600 # antennna integration time  
+
+simulated_clean = absorption(sim_amp, sim_x0, sim_width) + foreground(sim_coeffs)
+simulated = addnoise(simulated_clean, int_time)
+noise = simulated - simulated_clean
+
+def log_likelihood(cube): # log likelihood function for model parameters theta, simulated data, and model data
+    a0, a1, a2, a3, a4, amp, x0, width = cube # theta takes form of array of model parameters
+    coeffs = [a0,a1,a2,a3,a4]
+    model = absorption(amp, x0, width) + foreground(coeffs)
+    coeff = 1/(np.sqrt(2*pi*noise**2))
+    numerator = (simulated - model)**2 # likelihood depends on difference between model and observed temperature in each frequency bin
+    denominator = 2*noise**2
+    lj = coeff*np.exp(-numerator/denominator) 
+    return np.sum(np.log(lj)) # sum over all frequency bins
+
+def prior(cube):
+    cube[0]=10**(cube[0]*8 -5)
+    cube[1]=4*10**(cube[1]*8 -4)
+    cube[2]=5*10**(cube[2]*8 -2)
+    cube[3]=10**(cube[3]*8 - 2)
+    cube[4]=10**(cube[4]*8 - 2)
+    cube[5]=10**(cube[5]*8 - 3)
+    cube[6]=10**(cube[6]*4 + 3)
+    cube[7]=10**(cube[5]*4 + 3)
+    
+# number of dimensions our problem has
+parameters = ["a0", "a1", "a2", "a3", "a4", "amp", "x0", "width"]
+n_params = len(parameters)
+# name of the output files
+prefix = "testoutput1-"
+
+# run MultiNest
+result = solve(LogLikelihood=log_likelihood, Prior=prior, 
+	n_dims=n_params, outputfiles_basename=prefix, verbose=True)
+
+print()
+print('evidence: %(logZ).1f +- %(logZerr).1f' % result)
+print()
+print('parameter values:')
+for name, col in zip(parameters, result['samples'].transpose()):
+	print('%15s : %.3f +- %.3f' % (name, col.mean(), col.std()))
+
+# make marginal plots by running:
+# $ python multinest_marginals.py chains/3-
+# For that, we need to store the parameter names:
+import json
+with open('%sparams.json' % prefix, 'w') as f:
+	json.dump(parameters, f, indent=2)
+
