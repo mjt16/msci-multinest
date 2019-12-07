2bb46cb9d948b8dbc6678251d1b0ba1364990d00
Matthew Thomas <mjt16@ph-jpritcha-1.ph.ic.ac.uk>
2019-12-07 09:52:26 +0000

    fixed duplicate code in generate_simulated_data.py

--- class_implementation/generate_simulated_data.py
+++ class_implementation/generate_simulated_data.py
@@ -5,37 +5,17 @@ Created on Wed Nov 27 00:14:51 2019
 @author: matth
 """
 
-# runfile for multinest stuff
+# file to generate a set of noisy mock data according to a user-defined model
 
 # importing modules
 import model_database as md
 import implement_multinest as multi
 from math import pi
 import numpy as np
-import matplotlib.pyplot as plt  #this should be moved 
-
-freq = np.linspace(51.965332, 97.668457, 50) # frequency array for whole procedure
-
-# generating noisy data following a fiducial model, might want to move this code
-
-# functions
-def absorption(amp, x0, width): # signal 21cm absorption dip, defined as a negative gaussian
-    return -amp*np.exp((-(freq-x0)**2)/(2*width**2))
-
-def foreground(coeffs): # signal foreground
-    freq_0 = 78 # SORT THIS OUT!!!
-    l = len(coeffs)
-    p = np.arange(0,l,1)
-    freq_arr = np.transpose(np.multiply.outer(np.full(l,1), freq))
-    normfreq = freq_arr/freq_0
-    log_arr = np.log(normfreq)
-    pwrs = np.power(log_arr, p)
-    ctp = coeffs*pwrs
-    log_t = np.sum(ctp,(1)) 
-    temp = np.exp(log_t)
-    return temp
+import matplotlib.pyplot as plt
 
 # fiducial model parameters
+freq = np.linspace(51.965332, 97.668457, 50)
 a0 = 7.467527122442775
 a1 = -2.569712521825265
 a2 = -0.03731392522953101
@@ -46,19 +26,21 @@ sim_amp = 0.5 # amplitude
 sim_x0 = 78 # centre i.e. peak frequency
 sim_width = 8.1 # width
 int_time = 1.6e8 # antennna integration time  
+theta = [a0,a1,a2,a3,a4,sim_amp,sim_x0,sim_width]
 
-absorb = absorption(sim_amp, sim_x0, sim_width)
-foregd = foreground(sim_coeffs)
-clean = absorb + foregd
-noise = np.random.normal(0, 10e-2, len(freq)) #add noise
+mockmodel = md.logpoly_plus_gaussian(freq) # this is the user defined model, see model_database.py for more models
+noise = np.random.normal(0, 10e-2, len(freq))
 
-sim_signal = clean + noise
+sim_signal = mockmodel.observation(theta) + noise
+
+absorb = mockmodel.observation(theta, withFG=False) # seperating 21cm signal and foreground for plotting purposes
+foregd = mockmodel.observation(theta, withSIG=False)
 
 data = np.array([freq, absorb, foregd, noise, sim_signal])
 
-np.savetxt("sim_signal.txt", data, delimiter=",")
+np.savetxt("sim_signal.txt", data, delimiter=",") # save data in a .txt file; this mimics the procedure when actual data is imported e.g. EDGES
 
-#plotting
+# plotting mock signal and noise
 plt.subplot(1,2,1)
 plt.plot(freq, sim_signal, 'r-')
 plt.title("Simulated Signal")
