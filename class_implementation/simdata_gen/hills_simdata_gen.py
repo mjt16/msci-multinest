f306b90399222d9df86d17d4d3eb08cd08a5949d
Matthew Thomas <mjt16@ph-jpritcha-1.ph.ic.ac.uk>
2020-01-24 16:05:57 +0000

    updated simulated data folder

--- class_implementation/simdata_gen/hills_simdata_gen.py
+++ class_implementation/simdata_gen/hills_simdata_gen.py
@@ -21,15 +21,15 @@ a1 = -2.569712521825265
 a2 = -0.03731392522953101
 a3 = 0.049980346187051314
 a4 = 0.12055241703561778
-sim_coeffs = np.array([a0,a1,a2,a3,a4]) # simulated foreground coeffs
+a5 = -0.09089525886322
+sim_coeffs = np.array([a0,a1,a2,a3,a4,a5]) # simulated foreground coeffs
 sim_amp = 0.057 # amplitude
-sim_x0 = 78 # centre i.e. peak frequency
-sim_width = 8.1 # width
-sim_tau = 6.88 # optical depth
-int_time = 1.6e8 # antennna integration time  
-theta = [a0,a1,a2,a3,a4,sim_amp,sim_x0,sim_width,sim_tau]
+sim_phi = 5.74 
+sim_l = 12.27
+int_time = 1.6e8 # antenna integration time  
+theta = [a0,a1,a2,a3,a4,a5,sim_amp,sim_phi,sim_l]
 
-mockmodel = md.bowman(freq) # this is the user defined model, see model_database.py for more models
+mockmodel = md.hills_sine(freq) # this is the user defined model, see model_database.py for more models
 noise = np.random.normal(0, 10e-2, len(freq))
 
 sim_signal = mockmodel.observation(theta) + noise
@@ -39,7 +39,7 @@ foregd = mockmodel.observation(theta, withSIG=False)
 
 data = np.array([freq, absorb, foregd, noise, sim_signal])
 
-np.savetxt("sim_signal_bowman.txt", data, delimiter=",") # save data in a .txt file; this mimics the procedure when actual data is imported e.g. EDGES
+np.savetxt("sim_signal_hills.txt", data, delimiter=",") # save data in a .txt file; this mimics the procedure when actual data is imported e.g. EDGES
 
 # plotting mock signal and noise
 plt.subplot(1,2,1)
@@ -51,4 +51,4 @@ plt.subplot(1,2,2)
 plt.plot(freq, noise, 'bo')
 plt.title("Simulated Noise")
 plt.xlabel("Frequency/MHz")
-plt.savefig("mockdata_bowman.png")
+plt.savefig("mockdata_hills.png")
