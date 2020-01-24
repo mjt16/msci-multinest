1ac232daa072aa622969c1a975c73ec505c5113b
Madalina Tudorache <40742965+mnt116@users.noreply.github.com>
2020-01-24 21:21:18 +0000

    updated simdata analysis

--- class_implementation/simdata_gen/simdata_analysis.py
+++ class_implementation/simdata_gen/simdata_analysis.py
@@ -16,35 +16,38 @@ import model_database as md
 import itertools
 
 if len(sys.argv) != 2:
-	sys.stderr.write("""SYNOPSIS: %s <output-root> 
+	sys.stderr.write("""SYNOPSIS: %s <output-root>
 
-	output-root: 	Where the output of a MultiNest run has been written to. 
+	output-root: 	Where the output of a MultiNest run has been written to.
 	            	Example: chains/1-
 %s""" % (sys.argv[0], __doc__))
 	sys.exit(1)
 
 prefix = sys.argv[1]
 print('model "%s"' % prefix)
-if not os.path.exists(prefix + 'params.json'):
+if not os.path.exists(prefix + "/" + prefix + 'params.json'):
 	sys.stderr.write("""Expected the file %sparams.json with the parameter names.
 For example, for a three-dimensional problem:
 
 ["Redshift $z$", "my parameter 2", "A"]
 %s""" % (sys.argv[1], __doc__))
 	sys.exit(2)
-parameters = json.load(open(prefix + 'params.json'))
+
+model_runfile=import_module(prefix[:-1])
+
+parameters = json.load(open(prefix + "/" + prefix + 'params.json'))
 n_params = len(parameters)
 
-a = pymultinest.Analyzer(n_params = n_params, outputfiles_basename = prefix)
+a = pymultinest.Analyzer(n_params = n_params, outputfiles_basename = prefix + "/" + prefix)
 s = a.get_stats()
 
-json.dump(s, open(prefix + 'stats.json', 'w'), indent=4)
+json.dump(s, open(prefix + "/" + prefix + 'stats.json', 'w'), indent=4)
 
 print('  marginal likelihood:')
 print('    ln Z = %.1f +- %.1f' % (s['global evidence'], s['global evidence error']))
 print('  parameters:')
+errlist=[] #for error bars on plot
 paramlist = [] # for plotting model vs data
-errlist = [] # for error bars on plot
 for p, m in zip(parameters, s['marginals']):
 	lo, hi = m['1sigma']
 	med = m['median']
@@ -66,10 +69,13 @@ weights = a.get_data()[:,0]
 #mask = weights.cumsum() > 1e-5
 mask = weights > 1e-4
 
-corner.corner(data[mask,:], weights=weights[mask], 
+corner.corner(data[mask,:], weights=weights[mask],
 	labels=parameters, show_titles=True)
-plt.savefig(prefix + 'corner.pdf')
-plt.savefig(prefix + 'corner.png')
+
+if not os.path.exists(prefix[:-1] + "_results"):
+    os.mkdir(prefix[:-1] + "_results")
+plt.savefig(prefix[:-1] + "_results/" + prefix + 'corner.pdf')
+plt.savefig(prefix[:-1] + "_results/" + prefix + 'corner.png')
 plt.close()
 
 # IMPORTING MOCK DATA
@@ -80,9 +86,9 @@ noise = sim_stuff[3]
 absorb = sim_stuff[1]
 
 # GETTING CONVERGED MODEL
-model_signal = md.logpoly_plus_gaussian(freq)
+mymodel = model_runfile.my_model
 final_vals = numpy.array(paramlist)
-mod_signal = model_signal.observation(final_vals)
+mod_signal = mymodel.observation(final_vals)
 errors = numpy.array(errlist)
 errors = numpy.array(errors[5:8])
 sig_vals = numpy.array(final_vals[5:8])
@@ -124,4 +130,4 @@ plt.plot(freq, residuals, 'b-')
 plt.title("Residuals (full range)")
 plt.xlabel("Frequency/MHz")
 plt.subplots_adjust(wspace=0.3)
-plt.savefig("model_vs_data.png", dpi=200)
+plt.savefig(prefix+"model_vs_data.png", dpi=200)
