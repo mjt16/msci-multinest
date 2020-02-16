00f50416db6c55c82de4b91ccb33a843bd0545d4
Matthew Thomas <mjt16@ph-jpritcha-1.ph.ic.ac.uk>
2020-02-16 16:58:44 +0000

    another hills physical attempt

--- class_implementation/freenoise_hills_physical.py
+++ class_implementation/freenoise_hills_physical.py
@@ -36,7 +36,7 @@ def prior(cube): # priors for model parameters
    cube[0]= 10000*cube[0]
    cube[1]= -2*cube[1]
    cube[2]= -0.1 + 2*0.1*cube[2]
-   cube[3]= 0.2*cube[3]
+   cube[3]= -0.005 + 0.2005*cube[3]
    cube[4]= 200 + 1800*cube[4]
    cube[5]= 3*cube[5]
    cube[6]= 60 + 30*cube[6]
